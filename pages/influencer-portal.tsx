7ca00dc3a7a9e3f4e16a19f95ade882de65e2686
sidjcode <sidd2502@gmail.com>
2024-10-16 16:57:52 +0530

    Fix TypeScript error in handleDealSubmit function

--- pages/influencer-portal.tsx
+++ pages/influencer-portal.tsx
@@ -318,6 +318,7 @@ export default function InfluencerPortal() {
                                                                                     </TableRow>
                                                                                 ))}
                                                                             </TableBody>
+
                                                                         </Table>
                                                                     </CardContent>
                                                                 </Card>
