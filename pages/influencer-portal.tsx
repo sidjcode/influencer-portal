10a90eb08dc8d60e804284e9e3f4a7dee066bc6c
sidjcode <sidd2502@gmail.com>
2024-10-25 01:11:54 +0530

    portal

--- pages/influencer-portal.tsx
+++ pages/influencer-portal.tsx
@@ -795,15 +795,7 @@ export default function InfluencerPortal({ initialInfluencers, initialDeals, ini
                         {activeTab === "upload" && (
                             <>
                                 <h2 className="text-2xl font-bold mb-4">Upload Influencers</h2>
-                                <Card>
-                                    <CardHeader>
-                                        <CardTitle>Upload Influencers</CardTitle>
-                                        <CardDescription>Upload a CSV file containing influencer data</CardDescription>
-                                    </CardHeader>
-                                    <CardContent>
                                         <InfluencerUploader onUploadSuccess={fetchInfluencers} />
-                                    </CardContent>
-                                </Card>
                             </>
                         )}
                     </>
